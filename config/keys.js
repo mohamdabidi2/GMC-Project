f4580c3d34951eccb34e15d962048d545367f189
chemagh <44727828+chemagh@users.noreply.github.com>
2019-03-11 07:16:28 +0100

    2end

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
-module.exports={
-  mongoURI:'mongodb+srv://h3xor:794613@devexpe-1i4ix.mongodb.net/test?retryWrites=true'
-}
+module.exports = {
+  mongoURI: 'mongodb+srv://h3xor:794613@devexpe-1i4ix.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'secret'
+};
